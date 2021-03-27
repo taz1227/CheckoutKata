e9ce036b8d8aa32236de7bb83a4ca6e849bb72ac
Taran <taz1227@hotmail.co.uk>
2021-03-27 13:06:48 +0000

    Final changes including discounts

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -1,14 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CheckoutKata
 {
     public static class Checkout
     {
-        public static string Test(string t)
+        public static double ProcessOrder(string order )
         {
-            return t;        
+            // Populate Order object with values sent through
+            Order itemA = new Order
+            {
+                ItemSKU = "A",
+                Item = new Item { UnitPrice = 10},
+                Quantity = GetQuantityOfItem(order, 'A')
+            };
+
+            Order itemB = new Order
+            {
+                ItemSKU = "B",
+                Item = new Item { UnitPrice = 15 },
+                Quantity = GetQuantityOfItem(order, 'B')
+            };
+            Order itemC = new Order
+            {
+                ItemSKU = "C",
+                Item = new Item { UnitPrice = 40 },
+                Quantity = GetQuantityOfItem(order, 'C')
+            };
+            Order itemD = new Order
+            {
+                ItemSKU = "D",
+                Item = new Item { UnitPrice = 55 },
+                Quantity = GetQuantityOfItem(order, 'D')
+            };
+
+            List<Order> orderList = new List<Order>();
+
+            orderList.Add(itemA);
+            orderList.Add(itemB);
+            orderList.Add(itemC);
+            orderList.Add(itemD);
+
+            return BasketTotal(orderList);
         }
+
+        public static double BasketTotal(List<Order> order)
+        {
+            double totalPrice = 0;
+
+            foreach (var item in order)
+            {
+                // Apply 3 for 40 discount
+                if (item.ItemSKU == "B")
+                {
+                    // Discount Applied
+                    totalPrice = totalPrice + (GetDiscountForItemB(item) * 40);
+
+                    // Add remaining items of order
+                    totalPrice = totalPrice + ((item.Quantity % 3) * item.Item.UnitPrice);
+                }
+
+                // Apply 25% off for every 2 purchased together
+                else if (item.ItemSKU == "D")
+                {
+                    totalPrice = totalPrice + (item.Quantity * item.Item.UnitPrice - GetDiscountForItemD(item));
+                }
+
+                // Apply normal calculations for items A and C
+                else
+                {
+                    totalPrice = totalPrice + (item.Item.UnitPrice * item.Quantity);
+                }
+            }
+
+            return Math.Round(totalPrice, 2);
+        }
+
+        /// <summary>
+        /// Get quantity of each item given in the order
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="itemSKU"></param>
+        /// <returns></returns>
+        private static int GetQuantityOfItem(string order, char itemSKU)
+        {
+            return order.ToCharArray().Count(x => x == itemSKU);
+        }
+
+        /// <summary>
+        ///  Calculate the item for Item B
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private static int GetDiscountForItemB(Order item)
+        { 
+            return (item.Quantity - item.Quantity % 3) / 3;
+        }
+
+        /// <summary>
+        /// Calculate the discount for Item D
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private static double GetDiscountForItemD(Order item)
+        {
+            int mutiplesOf2InQuantity = (item.Quantity - item.Quantity % 2) / 2;
+
+            return mutiplesOf2InQuantity * item.Item.UnitPrice * 2 / 100.0 * 25.0;
+        }
+
     }
 }
--- CheckoutKata/UnitTest1.cs
+++ CheckoutKata/UnitTest1.cs
@@ -10,9 +10,51 @@ namespace CheckoutKata
         }
 
         [Test]
-        public void Test1()
+        public void TestEmptyOrder()
         {
-            Assert.AreEqual("Test", Checkout.Test("Test"));
+            Assert.AreEqual(0.00, Checkout.ProcessOrder(""));
+        }
+
+        [Test]
+        public void TestItemsThatDontExist()
+        {
+            Assert.AreEqual(0.00, Checkout.ProcessOrder("EFGH"));
+        }
+
+        [Test]
+        public void TestOneOfEachItem()
+        {
+            Assert.AreEqual(120, Checkout.ProcessOrder("ABCD"));
+        }
+
+        [Test]
+        [TestCase("BBB", 40)]
+        [TestCase("BBBB", 55)]
+        [TestCase("BBBBB", 70)]
+        [TestCase("BBBBBB", 80)]
+        public void TestItemBDiscount(string order, double expected)
+        {
+            Assert.AreEqual(expected, Checkout.ProcessOrder(order));
+        }
+
+        [Test]
+        [TestCase("DD", 82.50)]
+        [TestCase("DDD", 137.50)]
+        [TestCase("DDDD", 165)]
+        [TestCase("DDDDD", 220)]
+        public void TestItemDDiscount(string order, double expected)
+        {
+            Assert.AreEqual(expected, Checkout.ProcessOrder(order));
+        }
+
+        [Test]
+        [TestCase("AABBCCDD", 212.50)]
+        [TestCase("BBBCDDDDD", 300.00)]
+        [TestCase("AABBBBBBDDD", 237.50)]
+        [TestCase("ABCDABCDABCD", 327.50)]
+        public void TestMixedOrders(string order, double expected)
+        {
+            Assert.AreEqual(expected, Checkout.ProcessOrder(order));
         }
     }
 }
