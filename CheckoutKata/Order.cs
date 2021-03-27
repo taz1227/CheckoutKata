50d362861f5a9959f43464756763beb7e84f2dc7
Taran <taz1227@hotmail.co.uk>
2021-03-27 11:34:05 +0000

    Simple test for basic logic without discounts

---
+++ CheckoutKata/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKata
+{
+    public class Order
+    {
+        public string ItemSKU { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Item Item { get; set;  }
+    }
+}
